7d41370a63b3d5527e617733de1a7e69e560dcd9
SophiaL25 <122596592+SophiaL25@users.noreply.github.com>
2023-02-26 18:43:34 -0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,34 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaGFvY2gwNDIzIiwiYSI6ImNsZTd6ZWprZTAxOXAzdXFnN3J1NTVjZ2YifQ.75kz-3fG_A9SMXdoEZJiHg';
-
-  const map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
     container: 'map', // container ID
     style : 'mapbox://styles/haoch0423/clem0v58v000w01lfrh5gyln9',
     zoom: 11, // starting zoom
-    center: [-122.335167, 47.608013], // starting center
-})
+    center: [-122.385167, 47.608013], // starting center
+    projection: 'albers'
+});
+
+async function geojsonFetch() {
+    let response = await fetch('assets/Starbucks.json');
+    let covidRates = await response.json();
+
+    map.on('load', function loadingData() {
+        map.addSource('Starbucks.json', {
+            type: 'geojson',
+            data: covidRates
+        });
+
+        map.addLayer({
+            'id': 'Starbucks.json',
+            'type': 'fill',
+            'source': 'Starbucks.json',
+            'paint': {
+                'fill-color': '#FFEDA0',
+                'fill-outline-color': '#BBBBBB',
+                'fill-opacity': 0.7,
+            }
+        });
+
+    })
+}
+
+geojsonFetch();
