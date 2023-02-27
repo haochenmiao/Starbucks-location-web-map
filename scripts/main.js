37bb2fd7a6922c83e6e682b26f22aa2d72146220
SophiaL25 <122596592+SophiaL25@users.noreply.github.com>
2023-02-27 15:59:30 -0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,30 +2,33 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaGFvY2gwNDIzIiwiYSI6ImNsZTd6ZWprZTAxOXAzdXFnN
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style : 'mapbox://styles/haoch0423/clem0v58v000w01lfrh5gyln9',
-    zoom: 11, // starting zoom
-    center: [-122.385167, 47.608013], // starting center
+    zoom: 11.5, // starting zoom
+    center: [-122.325167, 47.608013], // starting center
     projection: 'albers'
 });
 
 async function geojsonFetch() {
-    let response = await fetch('assets/Starbucks.json');
-    let covidRates = await response.json();
-
+    // Load GeoJson asynchronously
+    let response, starbucks;
+    response = await fetch('assets/Starbucks.geojson');
+    starbucks = await response.json();
+    // Add map layers
     map.on('load', function loadingData() {
-        map.addSource('Starbucks.json', {
+        map.addSource('starbucks', {
             type: 'geojson',
-            data: covidRates
+            data: starbucks
         });
 
         map.addLayer({
-            'id': 'Starbucks.json',
-            'type': 'fill',
-            'source': 'Starbucks.json',
+            'id': 'starbucks-layer',
+            'type': 'circle',
+            'source': 'Starbucks',
             'paint': {
-                'fill-color': '#FFEDA0',
-                'fill-outline-color': '#BBBBBB',
-                'fill-opacity': 0.7,
-            }
+              'circle-radius': 8,
+              'circle-stroke-width': 2,
+              'circle-color': 'red',
+              'circle-stroke-color': 'white'
+          }
         });
 
     })
