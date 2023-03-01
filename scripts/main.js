35e5074ece934b304b79adc9aa71d1b21d2bfcc2
SophiaL25 <122596592+SophiaL25@users.noreply.github.com>
2023-02-28 16:45:30 -0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -31,7 +31,24 @@ async function geojsonFetch() {
           }
         });
 
-    })
+        map.on('click', (event) => {
+            const features = map.queryRenderedFeatures(event.point, {
+            layers: ['Starbucks-layer']
+            });
+            if (!features.length) {
+            return;
+            }
+            const feature = features[0];
+
+            const popup = new mapboxgl.Popup({ offset: [0, -15] })
+            .setLngLat(feature.geometry.coordinates)
+            .setHTML(
+            `<h3>${feature.properties.Name}</h3><p>${feature.properties.description}</p>`
+            )
+            .addTo(map);
+            });
+
+    });
 }
 
 geojsonFetch();
