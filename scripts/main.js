7c34cab1aba96a92dff756711cf3091611c31bd6
Haochen Miao <83092146+haochenmiao@users.noreply.github.com>
2023-03-09 22:39:35 -0800

    some changes on the listings

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,35 @@ const map = new mapboxgl.Map({
     projection: 'albers'
 });
 
+function getDistance(point1, point2) {
+    const R = 3958.8;
+    const lat1 = point1[1];
+    const lon1 = point1[0];
+    const lat2 = point2[1];
+    const lon2 = point2[0];
+    const dLat = deg2rad(lat2 - lat1);
+    const dLon = deg2rad(lon2 - lon1);
+    const a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) + 
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c;
+    return d;
+  }
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+}
+
 async function geojsonFetch() {
     // Load GeoJson asynchronously
     let response, Starbucks, driveThru, both;
     response = await fetch('assets/Starbucks_Seattle.geojson');
     Starbucks = await response.json();
+
+    response = await fetch('assets/in_store.geojson');
+    inStore = await response.json();
 
     response = await fetch('assets/drive_thru.geojson');
     driveThru = await response.json();
@@ -23,6 +47,11 @@ async function geojsonFetch() {
         map.addSource('Starbucks', {
             type: 'geojson',
             data: Starbucks
+        });
+
+        map.addSource('inStore', {
+            type: 'geojson',
+            data: inStore
         });
 
         map.addSource('driveThru', {
@@ -44,15 +73,31 @@ async function geojsonFetch() {
                 proximity: {
                     longitude: -122.3321,
                     latitude: 47.6062
-                }
+                },
+                countries: 'us',
+                types: 'poi'
             }),
             'top-left'
         );
+
+
         
         map.addLayer({
             'id': 'Starbucks-layer',
             'type': 'circle',
             'source': 'Starbucks',
+            'paint': {
+              'circle-radius': 5,
+              'circle-stroke-width': 2,
+              'circle-color': 'green',
+              'circle-stroke-color': 'white'
+          }
+        });
+
+        map.addLayer({
+            'id': 'inStore-layer',
+            'type': 'circle',
+            'source': 'inStore',
             'paint': {
               'circle-radius': 5,
               'circle-stroke-width': 2,
@@ -87,32 +132,54 @@ async function geojsonFetch() {
 
         map.on('click', (event) => {
             const features = map.queryRenderedFeatures(event.point, {
-            layers: ['Starbucks-layer', 'driveThru-layer', 'both-layer']
+            layers: ['Starbucks-layer', 'inStore-layer', 'driveThru-layer', 'both-layer']
             });
             if (!features.length) {
             return;
             }
             const feature = features[0];
-
+            const sourceData = map.getSource('Starbucks')._data;
+            
+            // Find the selected feature in the GeoJSON data
+            const selectedFeature = sourceData.features.find(f => f.properties.id === feature.properties.id);
+            
+            // Calculate the distance between the selected location and each location in the GeoJSON data
+            const distances = sourceData.features.map(f => {
+              const distance = getDistance(selectedFeature.geometry.coordinates, f.geometry.coordinates);
+              return { id: f.properties.id, distance: distance };
+            });
+            
+            // Sort the locations by distance
+            distances.sort((a, b) => a.distance - b.distance);
+            
+            // Create a HTML list of the locations with their distance from the selected location
+            let locationList = '<ul>';
+            distances.forEach(d => {
+              const location = sourceData.features.find(f => f.properties.id === d.id);
+              locationList += `<li><strong>${location.properties.Name}</strong> (${d.distance.toFixed(2)} miles)</li>`;
+            });
+            locationList += '</ul>';
+            
             // Create a new html element for the side panel
             const sidebar = document.getElementById('sidebar');
             const sidebarContent = document.querySelector('.sidebar-content');
-
-            sidebarContent.innerHTML = `<h3>${feature.properties.Name}</h3><p>${feature.properties.description}</p>`;
-
+            
+            sidebarContent.innerHTML = `<h3>${selectedFeature.properties.Name}</h3><p>${selectedFeature.properties.description}</p>${locationList}`;
+            
             sidebar.classList.add('open');
-
+            
             // Create the close button and add it to the sidebar
             const closeButton = document.createElement('button');
             closeButton.innerHTML = 'Close';
             closeButton.classList.add('sidebar-close');
             sidebar.appendChild(closeButton);
-
+            
             // Add an event listener to the close button
             closeButton.addEventListener('click', () => {
-            // Remove the "open" class from the sidebar element
-            sidebar.classList.remove('open');
+              // Remove the "open" class from the sidebar element
+              sidebar.classList.remove('open');
             });
+            
 
             const popup = new mapboxgl.Popup({ offset: [0, -15] })
             .setLngLat(feature.geometry.coordinates)
@@ -123,13 +190,19 @@ async function geojsonFetch() {
             });
 
         // Get the layer selector buttons by their ids
+        const allStoreSelector = document.getElementById('all-store-selector');
         const inStoreSelector = document.getElementById('in-store-selector');
         const driveThruSelector = document.getElementById('drive-thru-selector');
         const driveThruInStoreSelector = document.getElementById('drive-thru-in-store-selector');
 
         // Add click event listeners to the layer selector buttons
-        inStoreSelector.addEventListener('click', function() {
+        allStoreSelector.addEventListener('click', function() {
             toggleLayerVisibility('Starbucks-layer');
+            toggleSelectorActiveState(allStoreSelector);
+        });
+
+        inStoreSelector.addEventListener('click', function() {
+            toggleLayerVisibility('inStore-layer');
             toggleSelectorActiveState(inStoreSelector);
         });
 
